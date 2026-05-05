ab7691bdcf7e8cc5cce96a3daa8680affe8be3e9
cer049 <cer049@uark.edu>
2026-05-05 00:06:28 -0500

    Calculated G matrix on my personal computer

--- src/GenoAnalysis.R
+++ src/GenoAnalysis.R
@@ -1,72 +1,124 @@
 library(here)
 library(data.table)
 library(ASRgenomics)
-library(SNPRelate)
 library(AGHmatrix)
 library(dplyr)
 library(janitor)
 library(stringr)
+library(tibble)
+library(SNPRelate)
 
+# To create the genomic information matrix, we must use the SNPRelate package
+# hosted on Bioconductor
+
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("SNPRelate")
+
+#############################################################################
+
+# Local workflow to extract metadata and SNP doses while the cluster is down:
+
+# Reading snp file
+snpData <- fread(here("data/sandeepOnly", "sandeep1_numeric.txt"))
+snpData[1:10, 1:10]
+
+# Extracting metadata
+metadata <- snpData[, 2:5]
+snpData <- snpData[, -c(2:5)] # removing from original SNP dataset
+
+# Converting first column into row names
+snpData <- column_to_rownames(snpData, var = "snp")
+
+# Converting data frame to matrix for better memory usage:
+snpMatrix <- as.matrix(snpData)
+
+# Transposing matrix:
+snpMatrix <- t(snpMatrix)
+
+# Each row in metadata is a SNP, and we do not transpose it
+# Let's remove the whitespaces from metadata
+rownames(metadata) <- colnames(snpMatrix) # re-add snp names
+metadata <- apply(metadata, 1, str_trim)
+
+# Metadata may be saved as a data frame
+metadata <- as.data.frame(metadata)
+save(metadata, file = here("data", "metadata.RData"))
+
+# Before saving the matrix, let's convert the snp dosages to 0, 1, 2
+snpMatrix <- snpMatrix + 1
+save(snpMatrix, file = here("data", "snpMatrix.RData"))
+
+# Proceed to LD pruning section after this (some changes will be made there)
+
+#############################################################################
 
 #### Building genomic relationship matrix (based on shared genetic material - SNPs) ####
 # Read SNP data
 # We also want each SNP as a column and each accession as a row
 # This is the full SNP dataset with 2.9 million SNPs,
 # per https://www.frontiersin.org/journals/plant-science/articles/10.3389/fpls.2023.1172816/full
-snpData <- t(fread(here("data/sandeepOnly", "sandeep1_numeric.txt")))
-snpData[1:10, 1:10]
-dim(snpData)
+#snpData <- t(fread(here("data/sandeepOnly", "sandeep1_numeric.txt")))
+#snpData[1:10, 1:10]
+#dim(snpData)
 # The first five rows are metadata, while the remainder are genotypes
 
 # The very first row is the SNP names
 # The fifth row has only NAs, so it may be excluded
-unique(snpData[5, ])
-snpData <- snpData[-5, ]
+# unique(snpData[5, ])
+# snpData <- snpData[-5, ]
 
 # Promoting first row to column names
 # Now the column names correspond to SNP names
-snpData <- row_to_names(snpData, row_num = 1)
+#snpData <- row_to_names(snpData, row_num = 1)
 
 # Storing metadata in a separate data frame for later use
 # allele, chromosome and position information
-metadata <- snpData[1:3, ]
+#metadata <- snpData[1:3, ]
 # Remove whitespaces from metadata rows
-metadata <- apply(metadata, 2, str_trim)
+#metadata <- apply(metadata, 2, str_trim)
 
 # Saving metadata file
-save(metadata, file = here("data", "metadata.RData"))
+#save(metadata, file = here("data", "metadata.RData"))
 
 # Removing metadata rows from main SNP dataset
 # Removing spaces from SNP doses and turning them to numbers
-snpData <- snpData[-1:-3, ]
+#snpData <- snpData[-1:-3, ]
 
 # Remove leading and trailing whitespaces
-snpData <- apply(snpData, 2, str_trim)
+#snpData <- apply(snpData, 2, str_trim)
 
 # Convert SNP doses to numeric values
 # We want to keep row names intact, hence the list syntax
-snpData[] <- lapply(snpData, as.numeric)
+#snpData[] <- lapply(snpData, as.numeric)
 
 # Save the processed SNP data for later use
-save(snpData, file = here("data", "snpData.RData"))
+#save(snpData, file = here("data", "snpData.RData"))
 
+#--------------------------------- Extra --------------------------------------#
 # Extracting rownames from snpData to do some checking with the phenotypic data
 # Saving to memory
-snpAccessions <- rownames(snpData)
-save(snpAccessions, file = here("Thesis/Dry_DSR/CarlosReDo/data", "snpAccessions.RData"))
+#snpAccessions <- rownames(snpData)
+#save(snpAccessions, file = here("Thesis/Dry_DSR/CarlosReDo/data", "snpAccessions.RData"))
 
 #------------------------------------------------------------------------------------------------------------------------------#
-### LD pruning
+################################ LD pruning ###################################################
 # We want to remove SNPs that are in high linkage disequilibrium (LD) with each other, as they provide redundant information
 # Load original SNP dataset, as we will need metadata to perform LD pruning
 
 ## First of all, let's convert the SNP dosages to (0, 1, 2) format:
-load(file = here("data", "snpData.RData"))
+# load(file = here("data", "snpData.RData"))
+# snpData <- snpData + 1
 
-snpData <- snpData + 1
+# Loading metadata again:
+load(file = here("data", "metadata.RData"))
 
-# Saving snpData with updated dose values 
-save(snpData, file = here("data", "snpData.RData"))
+# And SNP matrix:
+load(file = here("data", "snpMatrix.RData"))
+
+# Saving snpData with updated dose values (maybe not necessary)
+# save(snpData, file = here("data", "snpData.RData"))
 
 ## Onto LD pruning itself:
 
@@ -74,15 +126,15 @@ save(snpData, file = here("data", "snpData.RData"))
 outGds <- here("output", "geno.gds")
 
 # Creating gds file
-snpgdsCreateGeno(outGds, genmat = snpData,
-                 sample.id = rownames(snpData),
-                 snp.id = colnames(snpData),
+snpgdsCreateGeno(outGds, genmat = snpMatrix,
+                 sample.id = rownames(snpMatrix),
+                 snp.id = colnames(snpMatrix),
                  snp.chromosome = metadata$chr,
                  snp.position = metadata$pos,
                  snpfirstdim = F)
 
 # File ready for pruning
-genofile <- snpgdsOpen(here("output", "outGds.gds"))
+genofile <- snpgdsOpen(outGds)
 
 # Defining an LD threshold of 0.8
 # When two SNPs have correlation coefficient greater than 0.8, one of them gets removed
@@ -91,20 +143,20 @@ sel_pruned <- snpgdsLDpruning(genofile, ld.threshold = thr, start.pos = "first",
 
 # List of selected SNPs after pruning
 # This list has 12 sublists, one for each chromosome
-# save(sel_pruned, file = here("output", "sel_pruned.RData"))
-load(file = here("output", "sel_pruned.RData"))
+save(sel_pruned, file = here("output", "sel_pruned.RData"))
+# load(file = here("output", "sel_pruned.RData"))
 
 # Get the list of selected SNPs after pruning
 # in a format that has all SNP names in a single vector/list of one dimension, instead of a list of vectors (one for each chromosome)
 sel_pruned <- unname(unlist(sel_pruned))
 
 # Now we filter snpData to keep only the pruned SNPs
-load(file = here("data", "snpData.RData"))
-snpPruned <- snpData[, sel_pruned]
+# load(file = here("data", "snpData.RData"))
+snpPruned <- snpMatrix[, sel_pruned]
 # snpPruned has the doses of all the SNPs that remained after pruning
 
 # Verifying the dimension of the pruned SNP dataset
-dim(snpPruned)
+# dim(snpPruned)
 
 # Saving it to memory
 save(snpPruned, file = here("output", "snpPruned.RData"))
@@ -112,20 +164,19 @@ save(snpPruned, file = here("output", "snpPruned.RData"))
 # The above SNP dataset is what we will use to build the genomic information matrix
 # Building the genomic information matrix with the pruned SNP dataset:
 
-load(file = here("output", "snpPruned.RData"))
+# load(file = here("output", "snpPruned.RData"))
 
 # No further pruning, so the minimum allele frequency (maf) is set to 0
 G <- Gmatrix(snpPruned, maf = 0)
 save(G, file = here("output", "G.RData"))
 
-
 #----------------------------------------------------------------------------------------#
 
 
 # The result below gives us an idea of the inbreeding coefficient
+sum(diag(G)/nrow(G)) - 1
 # It is close to 1, which means our population is highly inbred, or homozygous
 # Which is to be expected given the preferred mating system (selfing) of rice
-# sum(diag(G_matrix)/nrow(G_matrix)) - 1
 
 # In the heatmap of the G matrix, darker shades indicate more highly correlated individuals,
 # with specially darker shades along the diagonal, since it's individuals with themselves
