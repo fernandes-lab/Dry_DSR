837d29746c077375546e6bc9cb931712fca2ecc5
cer049 <cer049@uark.edu>
2026-05-27 17:05:22 -0500

    Continuation of file name change

---
+++ src/1-PhenoRagdoll.R
@@ -0,0 +1,189 @@
+library(here)
+library(asreml)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(tidyr)
+library(readxl)
+
+# According to the slides provided by Sandeep, there were 417 (RDP1) + 91 (3K RG) = 508 total accessions, 
+# meaning we have about 508 genotypes before any filtering
+
+#-------------------------------------------------------------------------------------------------------#
+#### Basic exploratory analysis and cleaning of the data ####
+
+# Experimental information dataset
+exp_ragdoll <- read.csv2(file = here("data/sandeepOnly/rawData", "Compiled_TestTube_data_means_V3.csv"), 
+                            header = TRUE, sep = ",")
+
+dim(exp_ragdoll)
+str(exp_ragdoll)
+summary(exp_ragdoll)
+# 1971 observations of 15 variables (in total)
+
+# Some variables need to be converted to meaningful types
+# Also changing to CamelBack notation for better organization
+# Furthermore, let's reorder the columns and rename some of them
+# Also keep everything lowercase
+# ML_means1 and RL_means1 are after removing outliers from the plots
+# I do not have access to the specific procedures so I will just remove both
+# SiteName and TrialType have only one level each, so it is pointless to keep them
+# The columns siteyear and replication represent the same information as well
+expRagdoll <- exp_ragdoll |>
+                # 'Genotype' becomes the first column
+                relocate(Genotype) |>
+                select(-c(SiteName, TrialType, ML_means1, RL_means1)) |>
+                mutate(across(Genotype:Row, as.factor)) |>
+                mutate(across(seed_count:SL_means, as.numeric)) |> 
+                rename(coleoptile = CL_means, mesocotyl = ML_means, rootlength = RL_means, shootlength = SL_means) |>
+                # Turn column names to lower case
+                rename_with(tolower) |>
+                rename(seedcount = seed_count, plotnum  = plotbarcode) |>
+                select(-siteyear)
+
+# Each plot corresponds to five seeds of a given genotype
+str(expRagdoll)
+summary(expRagdoll)
+
+# Checking how many times each genotype appears in each replication
+# Each genotype was supposed to be replicated 4 times
+with(expRagdoll, table(genotype, replication) > 0) |>
+        rowSums() |>
+        table()
+# We can see that 472 genotypes were replicated 4 times
+# The experiment is almost a perfect randomized complete block design (if each rep is to be treated as a block)
+# Also, accession is not necessarily the same as genotype, though it often is
+# Checking for NAs
+expRagdoll |>
+    is.na() |>
+    colSums()
+# There are 178 NAs in total, of which 133 come from the mesocotyl column and 24 from the coleoptile column, besides others
+# Reminder that there are 1971 observations of 10 variables in total (133 is less than 10% of 1971, for instance)
+
+# Checking distribution of variables
+png(here("output/plots","RagDollExpDists.png"), width=800, height=600)
+expRagdoll |>
+    pivot_longer(cols = 7:10) |>
+    ggplot(aes(x = value)) +
+    geom_density() +
+    facet_wrap(~name, scales = "free")
+dev.off()
+# Coleoptile and shootlength seem to have approximately normal distributions
+# Mesocotyl is right-skewed, and rootlength is bimodal
+
+# LS_means_ragdoll has the genotype IDs
+# Let's get the relevant columns from it
+# We need the IDs to link the ragdoll phenotype data to the genotypic data
+# Also renaming columns for easier understanding and manipulation
+genoInfo <- read_excel(here("data/sandeepOnly/rawData", "LS_means_ragdoll.xlsx")) |>
+                select(SEQ, Genotype, Sub_pop, 'HDRA genotype assay ID') |>
+                rename(genotype = Genotype, genonumber = SEQ, subpop = Sub_pop, genoID = 'HDRA genotype assay ID') |>
+                mutate(across(everything(), as.factor))
+
+# Correct some genotype names so they match the 
+# genotypic dataset (e.g. IRIS 313-11949 becomes IRIS_313-11949):
+genoInfo <- genoInfo |> 
+  mutate(genoID = as.factor(str_replace_all(genoID, "IRIS ", "IRIS_")))
+
+# Checking for NAs in genoInfo:
+genoInfo |>
+    is.na() |>
+    colSums()
+
+# Build indices to match the experimental dataset to the genotype codes dataset using
+# shared genotype names
+# "This guy in expRagdoll$genotype is in position target_idx in genoInfo$genotype" -> tgtIdx is a vector of such positions 
+tgtIdx <- match(expRagdoll$genotype, genoInfo$genotype)
+sum(is.na(tgtIdx))
+# There are 4 NAs, meaning 4 genotypes in expRagdoll could not be found among the 504 available in genoInfo
+
+# Add the genoID column to the expRagdoll dataset (for eventual use in GBLUP)
+expRagdoll <- expRagdoll |>
+                mutate(genoID = genoInfo$genoID[tgtIdx], subpop = genoInfo$subpop[tgtIdx])
+
+# Let's load the accessions to check if the experimental dataset has samples from all of them
+load(file = here("data", "snpAccessions.RData"))
+
+# Elements present in expRagdoll but not in the snp Accessions
+setdiff(unique(expRagdoll$genoID), snpAccessions)
+
+# Filter expRagdoll for only genotypes found in the snpAccessions dataset
+expRagdoll <- expRagdoll |>
+              filter(genoID %in% snpAccessions)
+
+# Dropping levels no longer represented:
+expRagdoll <- expRagdoll |>
+          droplevels()
+
+# Checking for NAs in the full consolidated experimental dataset
+expRagdoll |>
+    is.na() |>
+    colSums()
+# There is also a string "NA" category in genoID that we need to check for
+sum(expRagdoll == "NA", na.rm = T)
+# No element in expRagdoll has an "NA" string after the matching
+
+# That leaves us with 462 genotypes, which is not too bad
+# Considering the ceiling is 470
+
+# NAs only in the responses. The G matrix will hopefully bridge this gap
+# by leveraging the shared information across genotypes
+
+# Saving for posterity
+save(expRagdoll, file = here("data", "expRagdoll.Rdata"))
+
+# Let's also save genoInfo so it can be used again in PhenoField.r
+save(genoInfo, file = here("data", "genoInfo.Rdata"))
+
+#--------------------------------------------------------------------------------------------------#
+
+#### Using basic mixed models to extract heritability measures for each trait ####
+
+load(file = here("data", "expRagdoll.Rdata"))
+
+# In this case, our traits of interest are coleoptile, mesocotyl, rootlength and shootlength
+
+# Creating list of traits to analyze (CL_means through SL_means)
+traits <- colnames(expRagdoll)[colnames(expRagdoll) %in% c("coleoptile", "mesocotyl", "rootlength", "shootlength")]
+          
+### Estimating heritability with genotype as random for variance component extraction #### 
+
+## Useful to gauge whether genomic prediction (GP) is useful
+## (if heritability is low, GP might not be effective)
+
+# Heritability with Cullis method
+cullisHerit <- list()
+
+# Heritability in an alternative way
+altHerit <- list()
+
+# The classification will be by genoID since these values are the ones present in the genotypic dataset
+for (trait in traits) {
+    model <- asreml(as.formula(paste(trait, "~ replication")),
+                    random = ~ genoID, 
+                    residual = ~ idv(units), 
+                    data = expRagdoll)
+    pred <- predict(model, classify = "genoID")
+
+    ###  Cullis heritability calculation
+
+    # Average standard error of difference between the predicted means
+    avsed <- pred$avsed
+
+    # Average variance
+    aved <- avsed^2
+
+    # Variance component associated with the genoID variable
+    vg <- summary(model)$varcomp["genoID", "component"]
+
+    cullisHerit[[trait]] <- 1 - (aved / (2*vg))
+
+    ### Alternative way to calculate heritability - Based on error plot variance
+    # Divide by 4 due to the number of reps -> average residual error for a given genotype
+    altHerit[[trait]] <- vpredict(model, h2 ~ V1 / (V1 + (V2/4))) 
+}
+
+# Both heritability estimation methods yielded similar resuls to Sandeep's
+h2CullisRagdoll <- cullisHerit
+save(h2CullisRagdoll, file = here("output", "cullisHeritRagdoll.RData"))
+
---
+++ src/2-GBLUP.R
@@ -0,0 +1,395 @@
+library(here)
+library(asreml)
+library(dplyr)
+library(tidyr)
+
+# Loading functions from the functions folder
+sapply(list.files(path = here("functions"), 
+                  pattern = "\\.R$", full.names = T), source)
+
+# Setting a seed
+set.seed(199927)
+
+###############################################################
+##                Loading experimental data                  ##
+###############################################################
+
+# G matrix:
+load(here("output", "G.RData"))
+
+# Ragdoll experiment
+load(here("data", "expRagdoll.RData"))
+
+# Field experiment
+load(here("data", "expField.RData"))
+
+# Filtering field data for only "Deep" (> 8 cm) treatment
+expFieldDeep <- expField |> filter(depth == "Deep")
+
+# Loading Cullis heritability for predictive ability assessment
+# Note: heritability related to field emergence only
+load(file = here("output", "cullisHeritField.RData"))
+h2CullisEmerField <- h2CullisField$emergence
+rm(h2CullisField)
+
+################# Subpopulation Information ###################
+
+# Keeping subpopulation information for possible future uses
+# First we must map each genotype to a specific subpopulation
+
+# We will consolidate the genotype-subpop rows from both data sources 
+# into a single dataset
+popAux1 <- expField |>
+  select(genoID, subpop) |>
+  distinct(genoID, .keep_all = TRUE)
+
+popAux2 <- expRagdoll |>
+  select(genoID, subpop) |>
+  distinct(genoID, .keep_all = TRUE)
+
+genoPopMap <- merge(popAux1 |> select(genoID), popAux2, by = "genoID")
+rm(popAux1, popAux2)
+
+genoPopMap <- genoPopMap |>
+  rename(genotype = genoID) |>
+  droplevels()
+
+# Saving map to memory
+save(genoPopMap, file = here("output", "genoPopMap.RData"))
+
+###############################################################
+##                Adjusted means (first stage)               ##
+###############################################################
+
+# Here genotype is a fixed effect since we are interested in each genotype's
+# individual effect
+# Note: Sandeep claimed spacial correction didn't influence the results 
+# significantly, hence we can skip that
+
+#################### Field adjusted means ######################
+
+# Our only trait of interest in the field experiment is % emergence
+
+# Mixed model assuming block has no effect (the genotypes are not carried from
+# one block to another within a single replication, so estimating block effect
+# is meaningless)
+
+adjFieldEmerg <- adjMeans(expFieldDeep, "emergence", blck = "block")
+# save(adjFieldEmerg, file = here("output", "adjFieldEmerg.RData"))
+
+##################### Ragdoll adjusted means ##################
+
+# Our two traits of interest are mesocotyl and coleoptile length
+# Same procedure as for % emergence in the field experiment
+
+adjRagdollMeso <- adjMeans(expRagdoll, "mesocotyl")
+# save(adjRagdollMeso, file = here("output", "adjRagdollMeso.RData"))
+
+adjRagdollColeo <- adjMeans(expRagdoll, "coleoptile")
+# save(adjRagdollColeo, file = here("output", "adjRagdollColeo.RData"))
+
+###############################################################
+##                     GBLUP (second stage)                  ##
+###############################################################
+
+### Checking genetic variance structure:
+# sum(diag(G))/nrow(G)
+
+# A value well above 1 points to a high level of inbreeding in the population
+# which tracks because rice is predominantly self-pollinating
+
+### Heatmap illustrating the genetic covariance structure:
+# heatmap(G)
+# We can see big correlation regions
+# PCA would probably be interesting for this dataset
+# especially when conducting GWAS
+
+# List to store prediction accuracies for each modeling approach
+accs_List <- vector(mode = "list", length = 5)
+names(accs_List) <- c("accField", "accMesoIS", "accMT_IS", 
+                     "accIdx", "accIdxFixed")
+
+######################### Single-trait GP #####################
+
+#------------ Field emergence (standard selection) ----------#
+
+# -> This is our baseline model <-
+
+# Calling function that performs CV and returns a data frame with the GEBVs
+# and BLUEs
+
+GP_EmerField <- cv2stage(adjFieldEmerg, G, k = 5)
+
+# Predictive ability as the ratio between the correlation of GEBVs and BLUEs
+# and the Cullis heritability for emergence in the field
+accFieldEmergence <- cor(GP_EmerField$GEBV, GP_EmerField$BLUE)/
+  sqrt(h2CullisEmerField)
+
+accs_List[["accField"]] <- accFieldEmergence
+
+#------------ Ragdoll mesocotyl (indirect selection - IS) -----#
+
+# Coleoptile will be used for two-trait GP later
+
+# We will also have to eventually assess (for indirect selection)
+# how the GEBVs in the ragdoll experiment correlate with the BLUEs for
+# emergence in the field
+
+GP_MesoRagdoll <- cv2stage(adjRagdollMeso, G, k = 5)
+
+# To evaluate the prediction accuracy for the indirect selection approach,
+# we will assess the correlation between the lab mesocotyl GEBVs and the field 
+# emergence BLUEs. For that, we have to filter the genotypes so that only those
+# common to both datasets are left
+# Remember: our target trait is emergence, so we will divide by its heritability
+# (in the field)
+
+# Data frame with the common genotypes, plus the relevant GEBVs and BLUEs
+GP_MesoRagdoll <- merge(GP_MesoRagdoll |> select(genotype, GEBV), 
+              adjFieldEmerg |> select(genotype, BLUE), 
+              by = "genotype")
+
+# Calculating prediction accuracy:
+# Note: the value indicates that Sandeep only calculated the correlation
+# without dividing by Cullis heritability (he calculated predictive )
+accIS_MESO <- cor(GP_MesoRagdoll$GEBV, GP_MesoRagdoll$BLUE)/
+  sqrt(h2CullisEmerField) 
+
+accs_List[["accMesoIS"]] <- accIS_MESO
+
+######################### Multi-trait GP ########################
+
+## Basically a multi-trait indirect selection
+
+# To improve indirect selection, we will do multi-trait prediction with ragdoll
+# mesocotyl + coleoptile, mesocotyl being the primary trait
+
+# Assessing the correlation between coleoptile and mesocotyl in the ragdoll
+# experiment:
+# To assess the correlation between each of the above two traits and emergence 
+# in the field, let's consolidate the information in a single data frame
+MT_IS <- merge(adjRagdollMeso |> select(genotype, RagMeso = BLUE, 
+                                                wtMeso = weight),
+                       adjRagdollColeo |> select(genotype, RagColeo = BLUE,
+                                                 wtColeo = weight), 
+                       by = "genotype") |>
+  merge(adjFieldEmerg |> select(genotype, FieldEmer = BLUE,
+                                wtEmerg = weight), 
+        by = "genotype") |>
+        droplevels()
+
+# Even though mesocotyl has higher heritability than coleoptile, we will use 
+# mesocotyl as the primary trait (for CV) because its correlation with field
+# emergence is higher than the coleoptile's correlation with field emergence
+
+# First of all, for multi-trait asreml, the data must be in long format to
+# specify the weights correctly
+# We first remove the columns associated with the field experiment
+# Then pivot to longer format and finally pivot the result to wider format
+# In the end, we want a column for the trait, another for the BLUEs, and another
+# for the weights
+longMT_IS <- MT_IS |>
+            select(-c(FieldEmer, wtEmerg))
+
+# Auxiliary data frames to help organize the process of pivoting to long format
+# We will pivot each individually to long format, then merge them
+
+# Data frame with BLUEs only
+traitAux1 <- longMT_IS |>
+            select(genotype, RagMeso, RagColeo)
+
+traitAux1 <- traitAux1 |>
+            pivot_longer(
+              !genotype,
+              names_to = "trait",
+              # Captures only the "Meso" or "coleo" part
+              names_pattern = "Rag(.*)",
+              values_to = "BLUE"
+            )
+
+# Data frame with weights only
+traitAux2 <- longMT_IS |>
+  select(genotype, wtMeso, wtColeo)
+
+traitAux2 <- traitAux2 |>
+  pivot_longer(
+    !genotype,
+    names_to = "trait",
+    # Captures only the "Meso" or "coleo" part
+    names_pattern = "wt(.*)",
+    values_to = "weight"
+  )
+
+# Merging the data frames back into a single one
+longMT_IS <- merge(traitAux1, traitAux2, by = c("genotype", "trait"))
+rm(traitAux1, traitAux2)
+
+# Converting "trait" column to factor
+longMT_IS <- longMT_IS |>
+             mutate(trait = as.factor(trait))
+
+# Arrange the data so that all rows with a given trait are followed by all
+# rows with the other
+longMT_IS <- longMT_IS |>
+  arrange(trait)
+
+# From this point onwards, it is useful to have a function 
+# for cross-validation (CV)
+# We can use longMT_IS and the G matrix as arguments for the function
+GP_Meso_Coleo <- cv2stageMT(longMT_IS, G, k = 5)
+
+# Data frame with the common genotypes, plus the relevant GEBVs and BLUEs
+# FieldEmer represents the field emergence BLUEs
+GP_Meso_Coleo <- merge(GP_Meso_Coleo |> select(genotype, GEBV_Meso), 
+              MT_IS |> select(genotype, FieldEmer), 
+              by = "genotype")
+
+accIS_ML_CL <- cor(GP_Meso_Coleo$GEBV_Meso, GP_Meso_Coleo$FieldEmer)/
+  sqrt(h2CullisEmerField) 
+
+accs_List["accMT_IS"] <- accIS_ML_CL
+
+###################### Index variable GP ########################
+
+# We will use the same MT_ISdf dataset built for the multi-trait
+# genomic prediction approach
+# It contains the BLUEs for mesocotyl and coleoptile from the lab 
+# experiment, and for emergence in the field experiment
+
+# The criteria for choosing the best linear combination will be the
+# correlation between the GEBVs of the trait and field emergence
+
+# Remove columns related to the field experiment
+# and build a dataset to be fed into the index building
+# algorithm
+preIdx <- MT_IS |>
+          select(-c(FieldEmer, wtEmerg))
+
+# Weights to be tried for the traits
+wIdx <- seq(0, 1, by = 0.01)
+
+# Standardize the trait columns in preIdx
+# So their combination does not unfairly favor the one
+# with larger variance solely due to scale
+preIdx <- preIdx |>
+          mutate_at(c("RagMeso", "RagColeo"), function(x) scale(x))
+
+# The weight columns refer to the estimation errors when obtaining
+# BLUEs, so they will be kept the same
+
+# Vector to store prediction accuracies for each weight setup
+accs <- numeric()
+
+for (i in wIdx){
+  # Index variable
+  IdxVar <- i * preIdx$RagMeso + (1 - i) * preIdx$RagColeo
+  
+  # Index variable weight for GBLUP
+  wtIdx <- 1/((i^2)/preIdx$wtMeso + ((1 - i)^2)/preIdx$wtColeo)
+  
+  # Generating data frame to be fed to cv2stage function:
+  # The data frame must be in genotype-BLUE-weight format
+  IdxDF <- data.frame(genotype = preIdx$genotype,
+                      BLUE = IdxVar,
+                      weight = wtIdx)
+  
+  # Performing GBLUP with the index variable as response
+  IdxGBLUP <- cv2stage(IdxDF, G, k = 5)
+  
+  # Joining GBLUP dataset to field emergence BLUEs
+  IS_Idx <- merge(IdxGBLUP |> select(genotype, GEBV), 
+                adjFieldEmerg |> select(genotype, BLUE), 
+                by = "genotype")
+  
+  # Calculating prediction accuracy for indirect selection
+  # with index variable
+  accIS_Idx <- cor(IS_Idx$GEBV, IS_Idx$BLUE)/
+    sqrt(h2CullisEmerField) 
+  
+  accs <- c(accs, accIS_Idx)
+}
+
+# Obtaining best index weight combination
+bestIdxWt <- wIdx[which.max(accs)] # 0.6 mesocotyl / 0.4 coleoptile
+bestAcc <- max(accs)
+
+accs_List[["accIdx"]] <- bestAcc
+
+#------------------ Major QTL as fixed effect -------------------#
+
+# Note: this section makes use of the index variable built/selected
+# previously
+
+# The following code relies on the "3-GWAS.R" script
+# "mlid0051837994" was identified as a SNP with roughly
+# 10% as the percentage of variance explained (PVE)
+# Rex Bernardo's paper points out that QTLs with >=
+# 10% PVE and traits with around 80% heritability
+# provide minor improvements to the model
+
+
+# The construction of the new G matrix without the 
+# fixed effect SNP is performed in the "1-GenoAnalysis.R"
+# script
+
+# Loading the new G matrix
+load(file = here("output", "G_NoMajor.RData"))
+
+# The approach will be to use the index variable selected
+# above:
+
+IdxVar <- bestIdxWt * preIdx$RagMeso + (1-bestIdxWt) * preIdx$RagColeo
+
+wtIdx <- 1/((bestIdxWt^2)/preIdx$wtMeso + ((1-bestIdxWt)^2)/preIdx$wtColeo)
+
+IdxDF <- data.frame(genotype = preIdx$genotype,
+                    BLUE = IdxVar,
+                    weight = wtIdx)
+
+# Now we must add the column with the major SNP dosages across
+# the genotypes
+
+# Loading SNP dosage per genotype after pruning
+load(file = here("output", "snpPruned.RData"))
+
+# Column with only the major effect SNP
+snpMajor <- snpPruned[, colnames(snpPruned) == "mlid0051837994"]
+
+# Keeping the genotype information
+snpMajor <- cbind(rownames(snpPruned), snpMajor)
+colnames(snpMajor)[1] <- "genotype"
+rownames(snpMajor) <- NULL
+
+# Merging IdxDF to snpMajor
+IdxMajor <- merge(IdxDF, snpMajor, by = "genotype")
+
+# snpMajor column must be numeric
+IdxMajor <- IdxMajor |>
+            mutate(snpMajor = as.numeric(snpMajor))
+
+# Performing GBLUP with the index variable as response
+IdxGBLUP_Major <- cv2stageFixed(IdxMajor, G_NoMajor, k = 5)
+
+# Joining GBLUP dataset to field emergence BLUEs
+IS_IdxMajor <- merge(IdxGBLUP_Major |> select(genotype, GEBV), 
+                adjFieldEmerg |> select(genotype, BLUE), 
+                by = "genotype")
+
+# Calculating prediction accuracy for indirect selection
+# with index variable and major SNP fixed effect
+accIS_IdxMajor <- cor(IS_IdxMajor$GEBV, IS_IdxMajor$BLUE)/
+  sqrt(h2CullisEmerField) 
+
+accs_List[["accIdxFixed"]] <- accIS_IdxMajor
+
+# Accuracy remained basically the same...
+# Repeating CV multiple times within the respective functions 
+# would be ideal!!
+
+###############################################################
+##                    Saving model accuracies                ##
+###############################################################
+
+save(accs_List, file = here("output", "modelAccs.RData"))
+
+
